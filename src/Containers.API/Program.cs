4c19f38386b01afcf763b5ff3f4e490d9b4a2337
s31723 <s31723@pjwstk.edu.pl>
2025-04-18 17:22:22 +0200

    weird

--- src/Containers.API/Program.cs
+++ src/Containers.API/Program.cs
@@ -1,4 +1,14 @@
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using Containers.Application;
+using Containers.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("UniversityDatabase");
+
+builder.Services.AddTransient<IContainerService, IContainerService>(
+    _=> new ContainerService(connectionString));
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -36,9 +46,61 @@ app.MapGet("/weatherforecast", () =>
     .WithName("GetWeatherForecast")
     .WithOpenApi();
 
+app.MapGet("/api/containers", (IContainerService containerService) =>
+{
+    try
+    {
+        return Results.Ok(containerService.GetAllContainers());
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem(ex.Message);
+    }
+});
+
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+app.MapPost("/api/containers", (IContainerService containerService, Container container) =>
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+    try
+    {
+        var result = containerService.Create(container)
+        if (result is true)
+        {
+            return result.Created();
+        }
+        else
+        {
+            return Results.BadRequest();
+        }
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem(ex.Message);
+    }
+});
+
+app.MapPost("/api/containers", (IContainerService containerService, HttpRequest request) =>
+{
+    using (var reader = new StreamReader(request.Body))
+    {
+        string rawJson = reader.ReadToEnd();
+        
+        var json = await reader.ReadToEndAsync();
+
+        var json = JsonNode.Parse(rawJson);
+        var specifiedType = json["type"];
+        if (specifiedType != null && specifiedType.ToString() == "Standard")
+        {
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+            };
+            var containerInfo = JsonSerializer.Deserialize<Container>(json["typevalue"], options);
+            containerService.Create(new Container
+            {
+                Containertypeid = 
+            })
+        }
+    }
+});
--- src/Containers.Application/ContainerService.cs
+++ src/Containers.Application/ContainerService.cs
@@ -1,6 +1,71 @@
-﻿namespace Containers.Application;
+﻿using Containers.Models;
+using Microsoft.Data.SqlClient;
 
-public class ContainerService
+namespace Containers.Application;
+
+public class ContainerService : IContainerService
 {
+    private readonly String connectionString;
+
+    public ContainerService(String connectionString)
+    {
+        this.connectionString = connectionString;
+    }
+
+    public IEnumerable<Container> GetAllContainers()
+    {
+        List<Container> containers = [];
+        
+        
+        string query = "SELECT * FROM Containers";
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            SqlCommand command = new SqlCommand(query, connection);
+            
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            try
+            {
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        var container = new Container()
+                        {
+                            id = reader.GetInt32(0),
+                            Containertypeid = reader.GetInt32(1),
+                            isHazardious = reader.GetBoolean(2),
+                            Name = reader.GetString(3),
+                        };
+                        containers.Add(container);
+                    }
+                }
+            }
+            finally
+            {
+                reader.Close();
+            }
+        }
+       return containers;
+    }
     
+    public bool Create(Container container)
+    {
+        const string insertString = "INSERT INTO Containers (Containertypeid,isHazardous,Name) VALUES (@Containertypeid,@IsHazardous,@Name)";
+        int countRowsAdd = -1;
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            SqlCommand command = new SqlCommand(insertString, connection);
+
+            command.Parameters.AddWithValue("@Containertypeid", container.Containertypeid);
+            command.Parameters.AddWithValue("@IsHazardous", container.isHazardious);
+            command.Parameters.AddWithValue("@Name", container.Name);
+
+            connection.Open();
+            countRowsAdd = command.ExecuteNonQuery();
+        }
+        return countRowsAdd != -1;
+    }
 }
